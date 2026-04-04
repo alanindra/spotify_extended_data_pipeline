b0efe1081043255b74c6a43df14430dd78cb51a8
alanindra <qaulanmrf@gmail.com>
2026-04-04 16:43:23 +0700

    update queries

--- queries.py
+++ queries.py
@@ -24,23 +24,46 @@ class Queries:
     def query_extended_stream_table(self, config):
         """ 
         Creating a daily snapshot table of Spotify's extended stream data
+        Stream = audio, video
+        Stream must not be skipped (skipped = 0)
         Enrich the table with new cols, corresponding to config 
         """
 
-        query = f""" 
+        query = f"""
             select 
-                DATE(ts, '{config.utc_timezone}') AS date
+                ts timestamp
+                , DATE(ts, '{config.utc_timezone}') AS date
                 , STRFTIME('%H:%M', ts, '{config.utc_timezone}') AS time_hm
                 , STRFTIME('%H', ts, '{config.utc_timezone}') AS time_h
+                , ms_played
                 , ms_played / 1000 as sec_played
                 , ms_played / 60000 as min_played
-                -- , ms_played / 3600000 as hour_played
-                , *
+                , platform
+                , conn_country
+                , master_metadata_track_name
+                , master_metadata_album_artist_name
+                , master_metadata_album_album_name
+                , spotify_track_uri
+                , episode_name
+                , episode_show_name
+                , spotify_episode_uri
+                , audiobook_title
+                , audiobook_uri
+                , audiobook_chapter_uri
+                , audiobook_chapter_title
+                , reason_start
+                , reason_end
+                , shuffle
+                , skipped
+                , offline
+                , datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone}') offline_timestamp
+                , STRFTIME('%H:%M', datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone})) AS offline_time_hm
+                , STRFTIME('%H', datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone}')) AS offline_time_h
+                , incognito_mode
             from 
                 {config.extended_stream}
-                extended_stream_table
-            where skipped = 0 -- filter out skipped songs
-        """ 
+            where skipped = 0
+        """
 
         return query
     
