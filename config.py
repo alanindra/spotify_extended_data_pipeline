b2b86b3df8a090505c667d97d75ebf0c46bed9bf
alanindra <qaulanmrf@gmail.com>
2026-04-05 11:19:53 +0700

    update config

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 
 utc_timezone = "+7 hours"
-
 df_table_name_mapping = {
     "extended_audio_stream": "extended_audio_stream_table",
     "extended_video_stream": "extended_video_stream_table",
@@ -13,7 +12,6 @@ df_table_name_mapping = {
     "albums": "albums_table",
     "artist": "artist_table",
 }
-
 path = {
     "input": BASE_DIR / "input",
     "metadata": BASE_DIR / "input" / "metadata",
@@ -22,4 +20,14 @@ path = {
     "output": BASE_DIR / "output",
     "processed": BASE_DIR / "processed",
     "logs": BASE_DIR / "logs",
+}
+file_names = {
+    "stream": "StreamingHistory_music*",
+    "extended_stream": "Streaming_History*",
+    "metadata": "YourLibrary.json"
+}
+metadata_obejct = {
+    "tracks": "tracks",
+    "albums": "albums",
+    "artists": "artists"
 }
