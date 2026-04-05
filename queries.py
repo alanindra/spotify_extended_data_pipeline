9aad08f74ec9fa0c447791ad07e7f6b97bb91a57
alanindra <qaulanmrf@gmail.com>
2026-04-05 12:04:01 +0700

    add queries

--- queries.py
+++ queries.py
@@ -1,69 +1,55 @@
-import json
-import logging
+import config
 
-CONFIG_PATH = 'path'
+query = {
+    "stream_table":f""" 
+        select
+            endTime end_ts
+            , DATE(endTime, '{config.utc_timezone}') AS end_date
+            , STRFTIME('%H:%M', endTime, '{config.utc_timezone}') AS end_time_hm
+            , STRFTIME('%H', endTime, '{config.utc_timezone}') AS end_time_h
+            , artistName artist_name
+            , trackName track_name
+            , msPlayed ms_played
+            , msPlayed / 1000 as sec_played
+            , msPlayed / 60000 as min_played
+        from 
+            {config.df_table_name_mapping["stream"]}
+        where msPlayed <> 0
+    """,
 
-try:
-    with open(CONFIG_PATH, 'r') as file:
-        config = json.load(file)
-except FileNotFoundError:
-    logging.error("Configuration file not found.")
-    raise
-except Exception as e:
-    logging.error(f"Unexpected error occurred {e}.")
-    raise
-
-class Queries:
-    """  
-    List of queries to transform tables
+    "extended_stream_table":f""" 
+        select 
+            ts timestamp
+            , DATE(ts, '{config.utc_timezone}') AS date
+            , STRFTIME('%H:%M', ts, '{config.utc_timezone}') AS time_hm
+            , STRFTIME('%H', ts, '{config.utc_timezone}') AS time_h
+            , ms_played
+            , ms_played / 1000 as sec_played
+            , ms_played / 60000 as min_played
+            , platform
+            , conn_country
+            , master_metadata_track_name
+            , master_metadata_album_artist_name
+            , master_metadata_album_album_name
+            , spotify_track_uri
+            , episode_name
+            , episode_show_name
+            , spotify_episode_uri
+            , audiobook_title
+            , audiobook_uri
+            , audiobook_chapter_uri
+            , audiobook_chapter_title
+            , reason_start
+            , reason_end
+            , shuffle
+            , skipped
+            , offline
+            , datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone}') offline_timestamp
+            , STRFTIME('%H:%M', datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone}')) AS offline_time_hm
+            , STRFTIME('%H', datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone}')) AS offline_time_h
+            , incognito_mode
+        from 
+            {config.df_table_name_mapping["extended_stream"]}
+        where ms_played <> 0
     """
-
-    def __init__(self, config):
-        self.config = config
-
-    def query_extended_stream_table(self, config):
-        """ 
-        Creating a daily snapshot table of Spotify's extended stream data
-        Stream = audio, video
-        Stream must be longer than 0ms
-        Enrich the table with new cols, corresponding to config 
-        """
-
-        query = f"""
-            select 
-                ts timestamp
-                , DATE(ts, '{config.utc_timezone}') AS date
-                , STRFTIME('%H:%M', ts, '{config.utc_timezone}') AS time_hm
-                , STRFTIME('%H', ts, '{config.utc_timezone}') AS time_h
-                , ms_played
-                , ms_played / 1000 as sec_played
-                , ms_played / 60000 as min_played
-                , platform
-                , conn_country
-                , master_metadata_track_name
-                , master_metadata_album_artist_name
-                , master_metadata_album_album_name
-                , spotify_track_uri
-                , episode_name
-                , episode_show_name
-                , spotify_episode_uri
-                , audiobook_title
-                , audiobook_uri
-                , audiobook_chapter_uri
-                , audiobook_chapter_title
-                , reason_start
-                , reason_end
-                , shuffle
-                , skipped
-                , offline
-                , datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone}') offline_timestamp
-                , STRFTIME('%H:%M', datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone})) AS offline_time_hm
-                , STRFTIME('%H', datetime(offline_timestamp, 'unixepoch', '{config.utc_timezone}')) AS offline_time_h
-                , incognito_mode
-            from 
-                {config.extended_stream}
-            where ms_played <> 0
-        """
-
-        return query
-    
+}
